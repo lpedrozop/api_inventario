2777bb337da275e696decc6fab4b72b7cc9f930d
Lucas M. Pedrozo P <93496498+lpedrozop@users.noreply.github.com>
2022-11-03 10:00:37 -0500

    Update

--- src/controllers/inventario.controller.js
+++ src/controllers/inventario.controller.js
@@ -21,27 +21,24 @@ export const getInventario_ind = async (req, res) => {
       return res.status(500).json({message: 'Error de conexión'})
   }
 }
-
 export const createInventario = async (req, res) => {
-
-try{
-    const {id,title,author,category,detail,amount,purchase_price,sale_price} = req.body
-    const [rows] = await pool.query('INSERT INTO inventario (id,title,author,category,detail,amount,purchase_price,sale_price) VALUES (?,?,?,?,?,?,?,?)', [id,title,author,category,detail,amount,purchase_price,sale_price])
-    res.send({
+    try{
+        const {id,title,author,category,detail,amount,purchase_price,sale_price} = req.body
+        const [rows] = await pool.query('INSERT INTO inventario (id,title,author,category,detail,amount,purchase_price,sale_price) VALUES (?,?,?,?,?,?,?,?)', [id,title,author,category,detail,amount,purchase_price,sale_price])
+        res.send({
         id,title,author,category,detail,amount,purchase_price,sale_price
-
-    })
-} catch (error) {
-    return res.status(500).json({message: 'Error de conexión'})
-}
-
+        })
+    } catch (error) {
+        return res.status(500).json({message: 'Error de conexión'})
+    }
 }
 
 export const deleteInventario = async (req, res) => {
 try{
-    const [result] = await pool.query('DELETE FROM inventario WHERE id = ?', [req.params.id])
+    const {id} = req.body
+    const [result] = await pool.query('DELETE FROM inventario WHERE id = ?', [id])
     if(result.affectedRows <= 0) return res.status(404).json({
-        message:'Not found'})
+        message:'ID not found'})
     res.sendStatus(204)
 }catch (error) {
     return res.status(500).json({message: 'Error de conexión'})
@@ -50,8 +47,7 @@ try{
 
 export const updateInventario = async (req, res) => {
     try{
-        const {id} = req.params
-        const {amount, purchase_price, sale_price} = req.body
+        const {id,amount, purchase_price, sale_price} = req.body
         const [result] = await pool.query('UPDATE inventario SET amount = IFNULL(?,amount), purchase_price = IFNULL(?,purchase_price), sale_price = IFNULL(?,sale_price) WHERE id = ?',
         [amount,purchase_price, sale_price, id])
 
